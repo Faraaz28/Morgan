1d04a7a949df15815d7bf385ddbd4470094440c0
Faraaz.28 <faraazansari582@gmail.com>
2024-04-09 15:39:15 +0530

    hey

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+
 import Home from './pages/Home/Home';
+
+import Contact from './pages/Contact/Contact'
+
 import About from './pages/About/About';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -22,6 +27,13 @@ function App() {
           <Route path="/About">
             <About />
           </Route>
+
+          <Route path="/Contact">
+            <About />
+          </Route>
+
+
+          
 
         </Switch>
       </Router>
